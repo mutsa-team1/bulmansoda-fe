afe1b35c93c1cc841f940d9e1acc74c8b75107a4
twjin03 <twjindev@gmail.com>
2025-08-21 00:03:02 +0900

    feat: 장소 검색 구현

--- bulmansoda/src/App.jsx
+++ bulmansoda/src/App.jsx
@@ -1,9 +1,12 @@
-import KakaoMap from "./pages/KakaoMap";
+import SearchBar from "./components/SearchBar";
+// import KakaoMap from "./pages/KakaoMap";
+import MapPage from "./pages/MapPage";
 
 export default function App() {
+  
   return (
     <div className="w-screen h-[100dvh]">
-      <KakaoMap />
+      <MapPage/>
     </div>
   );
 }
---
+++ bulmansoda/src/components/SearchBar.jsx
@@ -0,0 +1,38 @@
+import { forwardRef } from "react";
+import searchIcon from "../assets/search.png";
+
+const SearchBar = forwardRef(function SearchBar({ onSearch }, ref) {
+  return (
+    <section
+      className="absolute top-3 left-3 right-3 z-10 
+                  flex items-center gap-2
+                bg-white rounded-2xl px-3 pl-3 py-2 shadow-lg"
+    >
+
+      <input
+        ref={ref}
+        className="flex-1 h-10 px-3 rounded-lg  
+                    placeholder:text-gray-400
+                    focus:outline-none "
+        placeholder="장소를 검색하세요."
+        onKeyDown={(e) => e.key === "Enter" && onSearch()}
+      />
+
+      <button
+        type="button"
+        onClick={onSearch}
+        className="h-[40px] px-3 rounded-lg font-medium 
+                    hover:bg-gray-100 active:bg-gray-200  ]"
+        aria-label="search"
+      >
+        <img
+          src={searchIcon}
+          alt="search"
+          className="h-[30px] w-[30px]"
+        />
+      </button>
+    </section>
+  );
+});
+
+export default SearchBar;
