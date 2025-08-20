8bb4ddba7d9dd718db079fa1f6b8f005c43c9295
twjin03 <twjindev@gmail.com>
2025-08-20 22:41:15 +0900

    feat: tailwind css 적용 및 api 호출 완료

--- bulmansoda/src/App.jsx
+++ bulmansoda/src/App.jsx
@@ -1,13 +1,9 @@
 import KakaoMap from "./pages/KakaoMap";
-import styled from 'styled-components';
-import MapPage from "./pages/MapPage";
 
 export default function App() {
   return (
-    <div style={{ padding: 16 }}>
-      {/* <h1>Vite + Kakao Map</h1> */}
-      {/* <KakaoMap /> */}
-      <MapPage/>
+    <div className="w-screen h-[100dvh]">
+      <KakaoMap />
     </div>
   );
 }
--- bulmansoda/src/pages/KakaoMap.jsx
+++ bulmansoda/src/pages/KakaoMap.jsx
@@ -1,16 +1,14 @@
-
 import { Map, MapMarker } from "react-kakao-maps-sdk";
-
-console.log("KEY =", import.meta.env.VITE_KAKAO_MAP_KEY);
-
 
 export default function KakaoMap() {
   const center = { lat: 37.5665, lng: 126.9780 };
+  const level = 4;
   return (
-    <div style={{ width: 402, height: 874 }}>
-      <Map center={center} style={{ width: "100%", height: "100%" }} level={12}>
-        {/* <MapMarker position={center} /> */}
-      </Map>
-    </div>
+    <Map
+      center={center}
+      style={{ width: "100%", height: "100%" }}
+      level={level}>
+      {/* <MapMarker position={center} /> */}
+    </Map>
   );
 }
